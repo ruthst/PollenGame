c0d435c946d94a6dd7402778e46d62177607f6ea
MarkMGray <markmgray2010@gmail.com>
2015-04-25 13:31:14 -0500

    added starting point for the pollen particle script

---
+++ ProjectPollen/Assets/Scripts/PollenParticle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+
+enum STATE {RAND, CONT};
+
+public class PollenParticle : MonoBehaviour {
+
+	Vector2 position;
+	Vector2 velocity;
+	Timer changeDirectionTimer;
+	Vector3 color;
+	STATE state;
+
+
+	PollenParticle(Vector2 _pos, Vector2 _vel)
+	{
+		position = _pos;
+		velocity = _vel;
+		changeDirectionTimer = new Timer(3000);
+		changeDirectionTimer.Elapsed += new ElapsedEventHandler(timerElapsed);
+		changeDirectionTimer.Enabled = true;
+		state = STATE.RAND;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void timerElapsed(object sender, ElapsedEventArgs e)
+	{
+		// Change pollen direction
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
