b99be5e801f053fa9dfd33e5704deabbb84474a3
Kaivan Wadia <kaivanwadia@gmail.com>
2015-04-25 20:38:34 -0500

    Chaining complete

--- ProjectPollen/Assets/Scripts/GameManager.cs
+++ ProjectPollen/Assets/Scripts/GameManager.cs
@@ -1,16 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public enum MSTATE {UNCHAIN, CHAIN};
 
 public class GameManager : MonoBehaviour {
 
+	public int maxPollen;
+	public GameObject pollenPrefab;
+	public Sprite[] pollenSprites;
+	public MSTATE mState;
+	public List<GameObject> pollenList;
+	public List<GameObject> currentChain;
 	// Use this for initialization
 	void Start () {
+		pollenList = new List<GameObject> ();
+		currentChain = new List<GameObject> ();
+		for (int i = 0; i < maxPollen; i++) {
+			float x = Random.Range(-4.9f, 4.9f);
+			float y = Random.Range(-8.9f, 8.0f);
+			GameObject pollen = (GameObject) Instantiate(pollenPrefab, new Vector3(x, y, 0), Quaternion.identity);
+			pollen.name = "pollen" + i;
+			int spriteNo = Random.Range(0,7);
+			pollen.GetComponent<PollenParticle>().color = (COLOR)spriteNo;
+			pollen.GetComponent<SpriteRenderer>().sprite = pollenSprites[spriteNo];
+			pollen.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-3.0f, 3.1f), Random.Range(-3.0f, 3.1f));
+			pollen.transform.parent = GameObject.Find("PollenList").transform;
+			pollenList.Add(pollen);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 mousePos = Input.mousePosition;
-		Debug.Log (mousePos);
+		if (pollenList.Count < maxPollen) {
+			for (int i = pollenList.Count; i < maxPollen; i++) {
+				float x = Random.Range(-4.9f, 4.9f);
+				float y = Random.Range(-8.9f, 8.0f);
+				GameObject pollen = (GameObject) Instantiate(pollenPrefab, new Vector3(x, y, 0), Quaternion.identity);
+				pollen.name = "pollen" + i;
+				int spriteNo = Random.Range(0,7);
+				pollen.GetComponent<PollenParticle>().color = (COLOR)spriteNo;
+				pollen.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-3.0f, 3.1f), Random.Range(-3.0f, 3.1f));
+				pollen.transform.parent = this.transform;
+				pollenList.Add(pollen);
+			}
+		}
+	}
 
+	void centerCollided(GameObject gObj){
+		Debug.Log ("Center Collided");
+		GameObject.Find ("WorkList").GetComponent<WorkList> ().BroadcastMessage ("move", this.currentChain.Count);
+		foreach (GameObject pollen in currentChain) {
+			Destroy(pollen);
+		}
+		this.currentChain.Clear ();
+		this.mState = MSTATE.UNCHAIN;
 	}
 }
--- ProjectPollen/Assets/Scripts/WorkList.cs
+++ ProjectPollen/Assets/Scripts/WorkList.cs
@@ -10,6 +10,7 @@ public class WorkList : MonoBehaviour {
 	public Sprite[] sprites;
 
 	public List<GameObject> listObjects;
+	public List<COLOR> currColorList;
 
 	public Vector3[] positions;
 
@@ -17,14 +18,9 @@ public class WorkList : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		listObjects = new List<GameObject>();
+		currColorList = new List<COLOR> ();
 		newWorkList();
 	}
-	
-	/*
-	Check if pollen has been absorbed in order, and how many
-	Filter down colors 
-	generate new ones at the end
-	*/
 
 	// Update is called once per frame
 	void Update () {
@@ -35,17 +31,12 @@ public class WorkList : MonoBehaviour {
 	Generate new list of pollen to be achieved
 	*/
 	void newWorkList() {
-		//get 5 random numbers mod 8, use those to get sprite
-
 		for (int i = 0; i < WORK_LIST_MAX; i++) {
-
 			int random = Random.Range(0,7);
-
-			//Debug.Log("The random is " + random);
+			currColorList.Add((COLOR)random);
 			listObjects.Add((GameObject) Instantiate(WorkListPrefab, positions[i], Quaternion.identity));
 			listObjects[i].GetComponent<SpriteRenderer>().sprite = sprites[random];
 		}
-
 	}
 
 	/*
@@ -53,7 +44,9 @@ public class WorkList : MonoBehaviour {
 	*/
 	void removeOldWork(int numDone){
 		for (int i = 0; i < numDone; i++) {
-			listObjects.RemoveAt(i);
+			Destroy(listObjects[0]);
+			listObjects.RemoveAt(0);
+			currColorList.RemoveAt(0);
 		}
 	}
 
@@ -62,30 +55,37 @@ public class WorkList : MonoBehaviour {
 	Shift worklist colors to the left
 	*/
 	void shiftWorkList(int numDone) {
-
-		foreach (GameObject elem in listObjects) {
-				elem.transform.localPosition = new Vector3(positions[listObjects.IndexOf(elem) - numDone].x, positions[listObjects.IndexOf(elem) - numDone].y, positions[listObjects.IndexOf(elem) - numDone].z);
+		for (int i = 0; i < listObjects.Count; i++) {
+			listObjects[i].transform.localPosition = positions[i];
 		}
+		//foreach (GameObject elem in listObjects) {
+		//		elem.transform.localPosition = new Vector3(positions[listObjects.IndexOf(elem) - numDone].x, positions[listObjects.IndexOf(elem) - numDone].y, positions[listObjects.IndexOf(elem) - numDone].z);
+		//}
 	}
-
 
 	/*
 	Generate new workList colors in empty slots
 	*/
 	void refillWorkList(int numDone) {
-		for (int i = (WORK_LIST_MAX - numDone); i < WORK_LIST_MAX; i++) {
+		for (int i = listObjects.Count; i < WORK_LIST_MAX; i++) {
 			int random = Random.Range(0,7);
-
+			currColorList.Add((COLOR)random);
 			listObjects.Add((GameObject) Instantiate(WorkListPrefab, positions[i], Quaternion.identity));
 			listObjects[i].GetComponent<SpriteRenderer>().sprite = sprites[random];
-
 		}
 	}
 
 	void move(int numDone) {
+		foreach (COLOR elem in currColorList) {
+			Debug.Log(elem);
+		}
+		Debug.Log ("dsajdsad");
 		removeOldWork(numDone);
 		shiftWorkList(numDone);
 		refillWorkList(numDone);
+		Debug.Log ("sdasdsad");
+		foreach (COLOR elem in currColorList) {
+			Debug.Log(elem);
+		}
 	}
-
 }
