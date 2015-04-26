b21917bb63fb6acffc93cf9bfccd6725b0d2669c
Kaivan Wadia <kaivanwadia@gmail.com>
2015-04-26 15:53:01 -0500

    Temp final build

--- ProjectPollen/Assets/Scripts/GameOverScript.cs
+++ ProjectPollen/Assets/Scripts/GameOverScript.cs
@@ -13,6 +13,8 @@ public class GameOverScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel("KaivanScene");
+		}
 	}
 }
