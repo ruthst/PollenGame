9f6bbb97159f20df2f1dea5ece945c99abb31b03
Kaivan Wadia <kaivanwadia@gmail.com>
2015-04-26 13:07:56 -0500

    Animations updating

---
+++ ProjectPollen/Assets/Scripts/EnableGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableGame : StateMachineBehaviour {
+	
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		GameObject.Find ("Main Camera").GetComponent<IntroMain> ().readyToStart = true;
+	}
+	
+//	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+//		Debug.Log ("OnStateUpdate");
+//	}
+//	
+//	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+//		Debug.Log ("OnStateExit");
+//	}
+//	
+//	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+//		Debug.Log ("OnStateMove");
+//	}
+//
+//	override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+//		Debug.Log ("OnStateIK");
+//	}
+}
---
+++ ProjectPollen/Assets/Scripts/IntroMain.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroMain : MonoBehaviour {
+	public bool readyToStart;
+
+	// Use this for initialization
+	void Start () {
+		this.readyToStart = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (this.readyToStart) {
+				Debug.Log("Here");
+				GameObject.Find("titleFlowerCenter").GetComponent<Animator>().SetBool("mouseClicked", true);
+				GameObject.Find("Title").GetComponent<Animator>().SetBool("mouseClicked", true);
+				GameObject.Find("beginText").GetComponent<Animator>().SetBool("mouseClicked", true);
+			}
+		}
+	}
+}
